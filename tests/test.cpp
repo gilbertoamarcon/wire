96f28b598cc4f1b82e94217e282f868f48719908
Gilberto Marcon <gilbertoamarcon@gmail.com>
2020-05-16 15:12:59 -0700

    Cleanup

--- tests/test.cpp
+++ tests/test.cpp
@@ -7,10 +7,10 @@ SCENARIO( "Pos can be incremented", "[inc]" ) {
 
 	GIVEN( "a new point" ) {
 
-		Geom::Pos a(2.0,1.0);
+		Geom::Pos a(6.0,1.0);
 		Geom::Pos b(3.0,4.0);
 
-		REQUIRE( a.x == 2.0 );
+		REQUIRE( a.x == 6.0 );
 		REQUIRE( a.y == 1.0 );
 		REQUIRE( b.x == 3.0 );
 		REQUIRE( b.y == 4.0 );
@@ -18,10 +18,37 @@ SCENARIO( "Pos can be incremented", "[inc]" ) {
 		WHEN( "increment a with b" ) {
 			a += b;
 			THEN( "have a = a+b" ) {
-				REQUIRE( a.x == 5.0 );
+				REQUIRE( a.x == 9.0 );
 				REQUIRE( a.y == 5.0 );
 			}
 		}
+
+		WHEN( "decrement a with b" ) {
+			a -= b;
+			THEN( "have a = a-b" ) {
+				REQUIRE( a.x ==  3.0 );
+				REQUIRE( a.y == -3.0 );
+			}
+		}
+
+		WHEN( "multiply inc a with b" ) {
+			a *= b;
+			THEN( "have a = a*b" ) {
+				REQUIRE( a.x == 18.0 );
+				REQUIRE( a.y ==  4.0 );
+			}
+		}
+
+		WHEN( "divide inc a with b" ) {
+			a /= b;
+			THEN( "have a = a/b" ) {
+				REQUIRE( a.x == 2.00 );
+				REQUIRE( a.y == 0.25 );
+			}
+		}
+
+
+
 
 	}
 
